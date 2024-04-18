73deadcc83627d70c6b6e00aab791abae0098644
Wagner Freitas <wagner.freitas@getconnect.com.br>
2024-04-18 11:46:24 -0300

    feat: rotinas de login no app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCBaXfK7J6UM7hLUhrfjKtO31Qqtu9zbjM",
+    authDomain: "sbmc-b0ae3.firebaseapp.com",
+    projectId: "sbmc-b0ae3",
+    storageBucket: "sbmc-b0ae3.appspot.com",
+    messagingSenderId: "696158341833",
+    appId: "1:696158341833:web:2d4e17f46f1f39bdf75b18",
+    measurementId: "G-7GQCY2WHCT"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyCBaXfK7J6UM7hLUhrfjKtO31Qqtu9zbjM",
+    authDomain: "sbmc-b0ae3.firebaseapp.com",
+    projectId: "sbmc-b0ae3",
+    storageBucket: "sbmc-b0ae3.appspot.com",
+    messagingSenderId: "696158341833",
+    appId: "1:696158341833:web:2d4e17f46f1f39bdf75b18",
+    measurementId: "G-7GQCY2WHCT"
+  }
 };
 
 /*
